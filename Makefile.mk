d1f9dd306b4a64b880b7021a709826f84e570275
Xiang Gong <xgong@ece.neu.edu>
2015-05-08 15:33:13 -0400

    Added MatrixTranspose binary

--- Makefile.mk
+++ Makefile.mk
@@ -1,7 +1,7 @@
 CC = gcc
 CXX = g++
 
-M2S_LIB = YOUR_PATH_TO_M2S_LIB
+M2S_LIB = /home/xgong/multi2sim-4.2/lib/.libs 
 
 INC = -I ../../include -I ../../include/SDKUtil
 LD = -pthread -L $(M2S_LIB) -lm2s-opencl -Wl,--no-as-needed -ldl  

1f85181ba2b2379c66d0e62431bfbb77a3abc4d6
Xiang Gong <xgong@ece.neu.edu>
2015-05-08 16:11:21 -0400

    Added M2c Kernel

--- Makefile.mk
+++ Makefile.mk
@@ -1,7 +1,7 @@
 CC = gcc
 CXX = g++
 
-M2S_LIB = /home/xgong/multi2sim-4.2/lib/.libs 
+M2S_LIB = /home/xgong/Develop/multi2sim-4.2/lib/.libs 
 
 INC = -I ../../include -I ../../include/SDKUtil
-LD = -pthread -L $(M2S_LIB)  -Wl,--no-as-needed,-Bdynamic -ldl -static -lm2s-opencl
+LD = -pthread -L $(M2S_LIB)  -Wl,--no-as-needed,-Bdynamic -static -lm2s-opencl

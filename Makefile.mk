16b9900f41e662f26cb45c87b6b87025fc3b58fa
Xiang Gong <xgong@ece.neu.edu>
2015-12-16 16:18:19 -0500

    Update

--- Makefile.mk
+++ Makefile.mk
@@ -1,7 +1,8 @@
 CC = gcc
 CXX = g++
 
-M2S_LIB = /home/xgong/Develop/multi2sim-4.2/lib/.libs 
+M2S_LIB = /home/xgong/multi2sim-4.2/lib/.libs 
+M2S_BIN = /home/xgong/multi2sim-4.2/bin/m2s 
 
 INC = -I ../../include -I ../../include/SDKUtil
 LD = -pthread -L $(M2S_LIB)  -Wl,--no-as-needed,-Bdynamic -static -lm2s-opencl
